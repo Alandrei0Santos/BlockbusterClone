e003c0da0fb318aa3ce26dd8b8010b9c61d4d7db
Alandrei0Santos <alandrei.santos@gmail.com>
2022-09-29 09:41:06 -0400

    Add interface and class to connect to MongoDB

---
+++ BlockbusterClone/Models/IBlockbusterDatabaseSettings.cs
@@ -0,0 +1,10 @@
+﻿namespace BlockbusterClone.Models
+{
+    public interface IBlockbusterDatabaseSettings
+    {
+        string BlockbusterCollection { get; set; }
+        string ConnectionString { get; set; }
+        string DatabaseName { get; set; }
+
+    }
+}
