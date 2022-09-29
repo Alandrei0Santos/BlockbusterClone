8afa4893fc2cd32096d2feb3e89e946977873a02
Alandrei0Santos <alandrei.santos@gmail.com>
2022-09-29 09:38:07 -0400

    Add Movies model

---
+++ BlockbusterClone/Models/Movies.cs
@@ -0,0 +1,27 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace BlockbusterClone.Models
+{
+    public class Movies
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; } = string.Empty;
+        
+        [BsonElement("name")]
+        public string Name { get; set; } = string.Empty;
+        
+        [BsonElement("synopsis")]
+        public string Synopsis { get; set; } = string.Empty;
+        
+        [BsonElement("genre")]
+        public string[] Genre { get; set; } = Array.Empty<string>();
+        
+        [BsonElement("director")]
+        public string Director { get; set; } = string.Empty;
+
+        [BsonElement("year")]
+        public int Year { get; set; }
+    }
+}
