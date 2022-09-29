352c991d9435f808a7e4dbd3e1d825146c307a6a
Alandrei0Santos <alandrei.santos@gmail.com>
2022-09-29 09:45:16 -0400

    Add MongoDB query functions

---
+++ BlockbusterClone/Services/BlockbusterServices.cs
@@ -0,0 +1,40 @@
+﻿using BlockbusterClone.Models;
+using MongoDB.Driver;
+
+namespace BlockbusterClone.Services
+{
+    public class BlockbusterServices : IBlockbusterServices
+    {
+        private readonly IMongoCollection<Movies> _blockbuster;
+
+        public BlockbusterServices(IBlockbusterDatabaseSettings settings, IMongoClient mongoClient)
+        {
+            var database = mongoClient.GetDatabase(settings.DatabaseName);
+            _blockbuster = database.GetCollection<Movies>(settings.BlockbusterCollection);
+        }
+        public Movies Create(Movies movies)
+        {
+            _blockbuster.InsertOne(movies);
+            return movies;
+        }
+
+        public List<Movies> GetMovies()
+        {
+            return _blockbuster.Find(movies => true).ToList();
+        }
+        public Movies GetDetails(string id)
+        {
+            return _blockbuster.Find(movies => movies.Id == id).FirstOrDefault();
+        }
+
+        public void Remove(string id)
+        {
+            _blockbuster.DeleteOne(movies => movies.Id == id);
+        }
+
+        public void Update(string id, Movies movies)
+        {
+            _blockbuster.ReplaceOne(movies => movies.Id == id, movies);
+        }
+    }
+}
---
+++ BlockbusterClone/Services/IBlockbusterServices.cs
@@ -0,0 +1,13 @@
+﻿using BlockbusterClone.Models;
+
+namespace BlockbusterClone.Services
+{
+    public interface IBlockbusterServices
+    {
+        List<Movies> GetMovies();
+        Movies GetDetails(string id);
+        Movies Create(Movies movies);
+        void Update(string id, Movies movies);
+        void Remove(string id);
+    }
+}
